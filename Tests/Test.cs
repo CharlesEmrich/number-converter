df4473fad8ab4904ce127e576f2fe5d79d2e2102
Adrian Agnic and Charles Emrich <adrian+charles@>
2017-06-01 11:07:18 -0700

    add failing test for teens

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -19,12 +19,25 @@ namespace NumberToWord
     }
 
     [Fact]
-    public void Convert_Number1to10_Eight()
+    public void Convert_Number1to9_Eight()
     {
       //arrange
       int testNumber = 8;
       NumberConverter test = new NumberConverter(testNumber);
       string expectedOutput = "eight";
+      //act
+      string output = test.Convert();
+      //assert
+      Assert.Equal(expectedOutput, output);
+    }
+
+    [Fact]
+    public void Convert_Number10to19_Fourteen()
+    {
+      //arrange
+      int testNumber = 14;
+      NumberConverter test = new NumberConverter(testNumber);
+      string expectedOutput = "fourteen";
       //act
       string output = test.Convert();
       //assert
